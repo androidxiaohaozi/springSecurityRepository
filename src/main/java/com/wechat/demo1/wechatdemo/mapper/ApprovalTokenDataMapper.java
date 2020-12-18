bcd1449a1b476101271f6907db99279da02f0fde
androidxiaohaozi <2360122978@qq.com>
2020-12-18 08:13:11 +0800

    first commit

---
+++ src/main/java/com/wechat/demo1/wechatdemo/mapper/ApprovalTokenDataMapper.java
@@ -0,0 +1,17 @@
+package com.wechat.demo1.wechatdemo.mapper;
+
+import com.wechat.demo1.wechatdemo.po.ApprovalTokenData;
+
+public interface ApprovalTokenDataMapper {
+    int deleteByPrimaryKey(String sp_no);
+
+    int insert(ApprovalTokenData record);
+
+    int insertSelective(ApprovalTokenData record);
+
+    ApprovalTokenData selectByPrimaryKey(String sp_no);
+
+    int updateByPrimaryKeySelective(ApprovalTokenData record);
+
+    int updateByPrimaryKey(ApprovalTokenData record);
+}
